aa3ad00ecab2467ab4fd79c3c0f677a884a22c26
sakai1230 <59103385+sakai1230@users.noreply.github.com>
2020-01-04 07:15:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+$('.yoko').show(7000);
+$('.tate').slideDown(7000);
+$('.containers').hover(
+  function(){
+    $('.slide').fadeIn();
+  },
+  function(){
+    $('.slide').fadeOut();
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
